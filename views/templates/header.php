77ed1c267964136889dd7adf95b045eab7c966bc
SCNeptune <steven.neptune@gmail.com>
2012-08-21 19:38:48 -0700

    Tried to do news tutorial
    starting from scratch and redoing everything

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,6 +1,8 @@
 <html>
 <head>
 	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
+	<script type="text/javascript" src="../../../libraries/jquery/jquery-1.8.0.min.js">
+	<script type="text/javascript" src="../../../libraries/"
 </head>
 <body>
 	<h1>CodeIgniter 2 Tutorial</h1>
